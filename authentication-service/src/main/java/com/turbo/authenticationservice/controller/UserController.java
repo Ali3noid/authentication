ccfce03dd986b6c6bac4a4fd0c11835c22f3679d
Rafal Nowakowski <rafal1nowakowski@gmail.com>
2018-06-19 20:35:13 +0200

    security - controllers - roles - additional functionality

---
+++ authentication-service/src/main/java/com/turbo/authenticationservice/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.turbo.authenticationservice.controller;
+
+import com.turbo.authenticationservice.exception.ResourceNotFoundException;
+import com.turbo.authenticationservice.model.User;
+import com.turbo.authenticationservice.payload.UserIdentityAvailability;
+import com.turbo.authenticationservice.payload.UserProfile;
+import com.turbo.authenticationservice.payload.UserSummary;
+import com.turbo.authenticationservice.repository.UserRepository;
+import com.turbo.authenticationservice.security.CurrentUser;
+import com.turbo.authenticationservice.security.UserPrincipal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        return new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+    }
+
+    @GetMapping("/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+        return new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
+    }
+}
